0fe9ef341d2b26aeb44e0d08c472e7cb3b57988d
biel.lil <biel1313biel@gmail.com>
2024-02-05 11:59:44 -0300

    revert: :fire: removing the InputComponent component
    This delicious dish has been temporarily retired due to ZOD's validation implementation with React Hook Form.

--- src/pages/Horarios/componests/input/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const InputComponentTs = styled.input`
-  margin-left: 1rem;
-`
-export const InputComponentTsYellow = styled.input`
-  margin-left: 1rem;
-  padding: 5px;
-`
