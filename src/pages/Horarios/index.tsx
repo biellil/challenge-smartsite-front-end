30d045e922691072150cec701903b4533980786f
biel.lil <biel1313biel@gmail.com>
2024-02-05 11:54:54 -0300

    feat: :sparkles: Addition of the 'zod' and 'react-hook-form' libraries to perform input validation.

--- src/pages/Horarios/index.tsx
+++ src/pages/Horarios/index.tsx
@@ -1,78 +1,109 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import iconTime from '../../assets/images/icon-hour.png'
-import InputComponent from './componests/input'
 import {
+  Button1,
+  Button2,
   FormContainer,
   Heroicon,
   LabelRadio,
   Labelcheckbox,
   SectionContainer,
+  SelectButton,
 } from './styles'
+
+const formSchema = z.object({
+  período: z.enum(['Manhã', 'Tarde', 'Noite']).optional().refine(Boolean, {
+    message: 'Selecione um período: manhã, tarde ou noite',
+  }),
+  unidades: z.boolean().optional(),
+})
+
+type FormData = z.infer<typeof formSchema>
+
 export function Horarios() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  })
+
+  function createHandleSubmit(data: FormData) {
+    console.log(data)
+  }
+
   return (
     <SectionContainer className="container">
       <Heroicon>
-        <img src={iconTime} />
+        <img src={iconTime} alt="Ícone de Horário" />
         <h2>Horário</h2>
       </Heroicon>
       <p>Qual período quer treinar?</p>
-      <FormContainer method="get">
-        <LabelRadio htmlFor="morning">
+      <FormContainer onSubmit={handleSubmit(createHandleSubmit)}>
+        <LabelRadio htmlFor="Manhã">
           <div>
-            <InputComponent
+            <input
               type="radio"
               id="Manhã"
-              name="schedule"
-              value="morning"
-              label="Manhã"
+              value="Manhã"
+              {...register('período')}
             />
             <span>Manhã</span>
           </div>
           <span>06:00 às 12:00</span>
         </LabelRadio>
-        <LabelRadio htmlFor="afternoon">
+
+        <LabelRadio htmlFor="Tarde">
           <div>
-            <InputComponent
+            <input
               type="radio"
-              id="afternoon"
-              name="schedule"
-              value="afternoon"
-              label="Tarde"
+              id="Tarde"
+              value="Tarde"
+              {...register('período')}
             />
             <span>Tarde</span>
           </div>
           <span>12:01 às 18:00</span>
         </LabelRadio>
-        <LabelRadio htmlFor="evening">
+
+        <LabelRadio htmlFor="Noite">
           <div>
-            <InputComponent
+            <input
               type="radio"
-              id="evening"
-              name="schedule"
-              value="evening"
-              label="Noite"
+              id="Noite"
+              value="Noite"
+              {...register('período')}
             />
             <span>Noite</span>
           </div>
           <span>18:01 às 23:00</span>
         </LabelRadio>
-        <Labelcheckbox htmlFor="closed-units">
+
+        <Labelcheckbox>
           <div>
-            <InputComponent
+            <input
               type="checkbox"
               id="closed-units"
-              label="Exibir unidades fechadas"
+              {...register('unidades')}
             />
             <span>Exibir unidades fechadas</span>
           </div>
-          <div>
-            <span> Resultados encontrados: </span>
-            <samp>0</samp>
-          </div>
+          <span>Encontrar unidade :0</span>
         </Labelcheckbox>
-        <div>
-          <button type="submit">ENCONTRAR UNIDADE</button>
-          <button type="reset">LIMPAR</button>
-        </div>
+
+        <SelectButton>
+          {errors.período && <p>{errors.período.message}</p>}
+          <div>
+            <Button1 type="submit">ENCONTRAR UNIDADE</Button1>
+            <Button2 type="reset" onClick={() => reset()}>
+              LIMPAR
+            </Button2>
+          </div>
+        </SelectButton>
       </FormContainer>
     </SectionContainer>
   )
--- src/pages/Horarios/styles.ts
+++ src/pages/Horarios/styles.ts
@@ -12,7 +12,7 @@ export const SectionContainer = styled.section`
   gap: 1rem;
   p {
     color: var(--light-grey);
-    font-size: 1.2rem;
+    font-size: 2rem;
     font-weight: 300;
     line-height: 1.5;
     margin: 0rem 1rem;
@@ -36,7 +36,7 @@ export const Heroicon = styled.div`
   h2 {
     line-height: 1.5;
     font-weight: normal;
-    font-size: 1.2rem;
+    font-size: 2rem;
     color: var(--light-grey);
   }
 `
@@ -54,7 +54,7 @@ export const FormContainer = styled.form`
   span {
     line-height: 1.5;
     font-weight: normal;
-    font-size: 1.2rem;
+    font-size: 1.5rem;
     color: var(--light-grey);
   }
 `
@@ -68,6 +68,71 @@ export const LabelRadio = styled.label`
     display: flex;
     align-items: center;
     gap: 0.5rem;
+
+    > span {
+      line-height: 1.5;
+      font-weight: normal;
+      font-size: 1.6rem;
+      color: var(--light-grey);
+    }
   }
 `
-export const Labelcheckbox = styled.label``
+export const Labelcheckbox = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > span {
+    line-height: 2;
+    font-weight: normal;
+    font-size: 1.4rem;
+    color: var(--dark-grey);
+  }
+  div {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    > span {
+      line-height: 2;
+      font-weight: normal;
+      font-size: 1.4rem;
+      color: var(--dark-grey);
+    }
+  }
+`
+
+export const SelectButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+
+    button {
+      margin-top: 1rem;
+      padding: 1rem 2.5rem;
+      width: 20rem;
+      border-radius: 8px;
+      line-height: 2;
+      font-weight: 900;
+      font-size: 1rem;
+      color: var(--dark-grey);
+      cursor: pointer;
+
+      &:hover {
+        box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.2);
+      }
+    }
+  }
+`
+export const Button1 = styled.button`
+  background: var(--yellow);
+`
+export const Button2 = styled.button`
+  background: white;
+  border: 2px solid var(--light-grey);
+`
